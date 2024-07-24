7ee67ee53e39105a6031209ed240e752e5fb068a
Lucas Faluba <falubalucas@gmail.com>
2024-07-24 12:27:34 -0300

    iniciado o projeto de criação de um sistema bibliotecário.

---
+++ Entities/Cliente.java
@@ -0,0 +1,75 @@
+package Entities;
+
+public class Cliente {
+
+	private String nome;
+	private int idade;
+	private String telefone;
+	private String livro;
+	
+	
+	//constructors
+	
+	public Cliente() {
+	}
+	
+	public Cliente(String nome, int idade, String telefone, String livro) {
+		super();
+		this.nome = nome;
+		this.idade = idade;
+		this.telefone = telefone;
+		this.livro = livro;
+	}
+	
+	//getters and setters
+	public String getNome() {
+		return nome;
+	}
+
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+
+	public int getIdade() {
+		return idade;
+	}
+
+
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+
+
+	public String getLivro() {
+		return livro;
+	}
+
+
+	public void setLivro(String livro) {
+		this.livro = livro;
+	}
+
+	//methods
+	 public String detalhes() {
+			return ("Nome: " + nome + 
+					"\nIdade: " + idade +
+					"\nTelefone: " + telefone+
+					"\nLivro: " + livro);
+	}
+	
+	public void reservarLivro() {
+		System.out.println("O livro " + livro + " foi reservado com sucesso!");
+	}
+}
---
+++ Program/Biblioteca.java
@@ -0,0 +1,87 @@
+package Program;
+
+import java.util.Scanner;
+
+import Entities.Cliente;
+
+public class Biblioteca {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println(
+				"-------------------------"
+				+"\n  SEJA BEM VINDO!"
+				+"\n-------------------------");
+		System.out.println("Você deseja entrar na área de:"
+							+ "\n1. Clientes"
+							+ "\n2. Colaboradores");
+		int option = sc.nextInt();
+		sc.nextLine();
+		
+		switch (option) {
+		
+		case 1:
+			
+			System.out.println("Vamos reservar seu livro!");
+			System.out.println("Vamos precisar de alguns dados seus, okay?");
+			System.out.println();
+			
+			System.out.println("Como você gostaria de ser chamado?");
+			String nome = sc.nextLine();
+			System.out.println();
+			
+			System.out.println("Quantos anos você tem?");
+			int idade = sc.nextInt();
+			sc.nextLine();
+			
+			System.out.println();
+			System.out.println("Insira seu telefone de contato no formato (ddd)9xxxx-xxxx");
+			String telefone = sc.nextLine();
+			System.out.println();
+			
+			System.out.println("Digite o nome do livro desejado: ");
+			String livro = sc.nextLine();
+			System.out.println();
+			System.out.println();
+			
+			Cliente[] cl = new Cliente[10];
+			cl[0] = new Cliente(nome, idade, telefone, livro);
+			
+			cl[0].reservarLivro();
+			
+			System.out.println();
+			
+			System.out.println("Informações do Cliente"
+					+"\n\n" +
+					cl[0].detalhes());
+			break;
+			
+			
+		case 2:
+			break;
+		}
+		
+			
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		sc.close();
+	}
+ 
+}
